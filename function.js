ee964135b818dc7131384775deca2ed9a95f0b0f
frostfry <159707119+frostfry@users.noreply.github.com>
2024-02-17 17:04:35 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -1,4 +1,10 @@
+//Light Mode - Dark Mode body and text toggle//
 function myFunction() {
     var element = document.body;
     element.classList.toggle("light-mode");
+    var element = document.getElementById("myButton");
+    element.classList.toggle("button-dark-mode")
+    var btn = document.getElementById("myButton");
+    if (btn.innerText=="Dark Mode") btn.innerText = "Light Mode";
+    else (btn.innerText = "Dark Mode");   
 }
